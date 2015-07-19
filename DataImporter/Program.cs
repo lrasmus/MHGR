47d54c5259b04399482e7a02b237e14b44e29973
Luke Rasmussen <luke.rasmussen@gmail.com>
2015-07-19 11:26:13 -0500

    Added consistency checks for phenotype data load

--- DataImporter/Program.cs
+++ DataImporter/Program.cs
@@ -15,6 +15,16 @@ namespace DataImporter
         {
             var phenotypeData = File.ReadAllLines(ConfigurationManager.AppSettings["PhenotypeData"]);
             PhenotypeLoader.Run(phenotypeData);
+            if (!PhenotypeLoader.ConsistencyChecks(17, 996, 5000, 5000, 5000))
+            {
+                Console.WriteLine("FAILED - Results of the phenotype load do not match internal consistency checks.");
+                Console.WriteLine("         Please resolve issues before proceeding with other data loads.");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Passed - Consistency checks passed for phenotype data load");
+            }
         }
     }
 }
--- DataImporter/Relational/PhenotypeLoader.cs
+++ DataImporter/Relational/PhenotypeLoader.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MHGR.Models.Relational;
+using System.Data.Entity;
 
 namespace DataImporter.Relational
 {
@@ -21,6 +22,30 @@ namespace DataImporter.Relational
                 var patient = patientRepo.Upsert(fields[0], fields[1], fields[2], fields[3], DateTime.Parse(fields[4]));
                 phenotypeRepo.AddResult(patient, fields[5], fields[6], fields[7], fields[8], fields[9], DateTime.Parse(fields[10]));
             }
+        }
+        
+        private static bool CheckEntityCounts(int expectedCount, int actualCount, string entityName)
+        {
+            if (actualCount != expectedCount)
+            {
+                Console.WriteLine("Expected {0} {1}, but counted {2}", expectedCount, entityName, actualCount);
+                return false;
+            }
+
+            return true;
+        }
+
+        public static bool ConsistencyChecks(int phenotypes, int patients, int patientPhenotypes, int patientResultCollections, int patientResultMembers)
+        {
+            var entities = new RelationalEntities();
+            bool isValid = true;
+            isValid = isValid && CheckEntityCounts(phenotypes, entities.phenotypes.Count(), "phenotypes");
+            isValid = isValid && CheckEntityCounts(patients, entities.patients.Count(), "patients");
+            isValid = isValid && CheckEntityCounts(patientPhenotypes, entities.patient_phenotypes.Count(), "patient phenotypes");
+            isValid = isValid && CheckEntityCounts(patientResultCollections, entities.patient_result_collections.Count(), "patient result collections");
+            isValid = isValid && CheckEntityCounts(patientResultMembers, entities.patient_result_members.Count(), "patient result members");
+
+            return isValid;
         }
     }
 }
