622334e63ee0552600acafd6d4701b37ceab924b
Luke Rasmussen <luke.rasmussen@gmail.com>
2015-08-12 16:56:13 -0500

    Integrated VCF parser

---
+++ DataImporter/Hybrid/VCFLoader.cs
@@ -0,0 +1,139 @@
+﻿using Bio.VCF;
+using MHGR.Models.Hybrid;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MHGR.DataImporter.Hybrid
+{
+    public class VCFLoader : BaseLoader
+    {
+        public VariantRepository variantRepo = new VariantRepository();
+        public PatientRepository patientRepo = new PatientRepository();
+        public SourceRepository sourceRepo = new SourceRepository();
+
+        public override void LoadData(string[] data)
+        {
+            throw new NotImplementedException("This method is not implemented, please pass a file path to LoadData");
+        }
+
+        private patient_variant_information AddFeatureInformation(string attributeName, string attributeValue)
+        {
+            var infoType = variantRepo.AddVariantInformationType(attributeName, null, Enums.VariantInformationTypeSource.VCF);
+            return new patient_variant_information()
+            {
+                item_type = Enums.PatientVariantType.SNP,
+                type_id = infoType.id,
+                value = attributeValue
+            };
+        }
+
+        private void SetVariantValues(patient_variants variant, VariantContext context)
+        {
+            if (context.AlternateAlleles.Count >= 1)
+            {
+                variant.value1 = context.AlternateAlleles[0].DisplayString;
+                if (context.AlternateAlleles.Count >= 2)
+                {
+                    variant.value2 = context.AlternateAlleles[1].DisplayString;
+                }
+                else
+                {
+                    variant.value2 = variant.value1;
+                }
+            }
+        }
+
+        private patient_variant_information AddPragmaInformation(string pragmaName, string pragmaValue)
+        {
+            var infoType = variantRepo.AddVariantInformationType(pragmaName, null, Enums.VariantInformationTypeSource.VCF);
+            return new patient_variant_information()
+            {
+                item_type = Enums.PatientVariantType.Collection,
+                type_id = infoType.id,
+                value = pragmaValue
+            };
+        }
+
+        public void LoadData(string filePath)
+        {
+            var vcfParser = new VCFParser(filePath);
+            var header = vcfParser.Header;
+            var collectionInformationList = new List<patient_variant_information>();
+            // We pull out all of the metadata from the header (all lines) and write them as information
+            // lines associated with this result.
+            foreach (var headerItem in header.MetaDataInInputOrder)
+            {
+                collectionInformationList.Add(AddPragmaInformation(string.Format("VCF:{0}", headerItem.Key), headerItem.Value));
+            }
+
+            var patient = patientRepo.AddPatient("DUMMY", "EHR", "Dummy", "VCF", null);
+
+            var reference = header.MetaDataInInputOrder.First(x => x.Key == "reference").Value;
+            DateTime? resultDate = null; // DateTime.Parse(header.MetaDataInInputOrder.First(x => x.Key == "fileDate").Value);
+            var patientVariants = new List<patient_variants>();
+            var featureInformationList = new Dictionary<patient_variants, List<patient_variant_information>>();
+            while (vcfParser.MoveNext())
+            {
+                var current = vcfParser.Current;
+                var variant = variantRepo.AddVariant(null, current.ID, "dbSNP",
+                    current.Chr, current.Start, current.End,
+                    reference, current.Reference.BaseString);
+                var patientVariant = new patient_variants()
+                {
+                    patient_id = patient.id,
+                    reference_id = variant.id,
+                    resulted_on = resultDate,
+                    variant_type = Enums.PatientVariantType.SNP
+                };
+                SetVariantValues(patientVariant, current);
+                patientVariants.Add(patientVariant);
+
+                var attributeList = new List<patient_variant_information>();
+                foreach (var attribute in current.Attributes)
+                {
+                    attributeList.Add(AddFeatureInformation(string.Format("VCF:{0}", attribute.Key), attribute.Value.ToString()));
+                }
+
+                if (current.FiltersMaybeNull != null)
+                {
+                    foreach (var filter in current.FiltersMaybeNull)
+                    {
+                        attributeList.Add(AddFeatureInformation("VCF:Filter", filter));
+                    }
+                }
+
+                foreach (var genotype in current.Genotypes)
+                {
+                    attributeList.Add(AddFeatureInformation("VCF:Genotype", genotype.ToString()));
+                }
+                featureInformationList.Add(patientVariant, attributeList);
+            }
+
+            // Save the collection to get its ID
+            var source = sourceRepo.AddSource("VCF", "VCF file");
+            var collection = patientRepo.AddCollection(patient, source);
+
+            // Save the collection-level header data
+            collectionInformationList.ForEach(x => x.item_id = collection.id);
+            variantRepo.AddPatientVariantInformationList(collectionInformationList);
+            variantRepo.AddPatientVariants(patientVariants);
+
+            // Save the individual attributes associated with each feature.
+            // Must be done after the patient variants are written to DB (above), since we
+            // rely on the ID being set.
+            foreach (var pair in featureInformationList)
+            {
+                foreach (var attribute in pair.Value)
+                {
+                    attribute.item_id = pair.Key.id;
+                }
+                variantRepo.AddPatientVariantInformationList(pair.Value);
+            }
+
+            variantRepo.AddPatientVariantsToCollection(collection, patientVariants);
+        }
+    }
+}
--- DataImporter/Program.cs
+++ DataImporter/Program.cs
@@ -1,4 +1,5 @@
-﻿using MHGR.DataImporter.Hybrid;
+﻿using Bio.VCF;
+using MHGR.DataImporter.Hybrid;
 using System;
 using System.Configuration;
 using System.IO;
@@ -54,23 +55,26 @@ namespace DataImporter
             //    Console.WriteLine("Passed - Consistency checks passed for star variant data load");
             //}
 
-            var gvfLoader = new GVFLoader();
-            string[] files = Directory.GetFiles(ConfigurationManager.AppSettings["GVFDataPath"], ConfigurationManager.AppSettings["GVFDataFilter"]);
-            foreach (var file in files)
-            {
-                var gvfData = File.ReadAllLines(file);
-                gvfLoader.LoadData(gvfData);
-            }
-            if (!gvfLoader.ConsistencyChecks(1000, 18, 8, 53, 5000, 67000, 8000, 72000, 187000, 23))
-            {
-                Console.WriteLine("FAILED - Results of the GVF load do not match internal consistency checks.");
-                Console.WriteLine("         Please resolve issues before proceeding with other data loads.");
-                return;
-            }
-            else
-            {
-                Console.WriteLine("Passed - Consistency checks passed for GVF data load");
-            }
+            //var gvfLoader = new GVFLoader();
+            //string[] files = Directory.GetFiles(ConfigurationManager.AppSettings["GVFDataPath"], ConfigurationManager.AppSettings["GVFDataFilter"]);
+            //foreach (var file in files)
+            //{
+            //    var gvfData = File.ReadAllLines(file);
+            //    gvfLoader.LoadData(gvfData);
+            //}
+            //if (!gvfLoader.ConsistencyChecks(1000, 18, 8, 53, 5000, 67000, 8000, 72000, 187000, 23))
+            //{
+            //    Console.WriteLine("FAILED - Results of the GVF load do not match internal consistency checks.");
+            //    Console.WriteLine("         Please resolve issues before proceeding with other data loads.");
+            //    return;
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Passed - Consistency checks passed for GVF data load");
+            //}
+
+            var vcfLoader = new VCFLoader();
+            vcfLoader.LoadData(ConfigurationManager.AppSettings["VCFData"]);
         }
     }
 }
