648b1aa93e7ff2149d9599a7db117797bc9c1dbb
Luke Rasmussen <luke.rasmussen@gmail.com>
2015-09-03 16:02:14 -0500

    Enrich the sequence region data for GVFs

--- DataImporter/GVF/PragmaParser.cs
+++ DataImporter/GVF/PragmaParser.cs
@@ -18,6 +18,18 @@ namespace MHGR.DataImporter.GVF
                 Name = pragmaParts[0]
             };
 
+            // The sequence region pragma has values without keys, so we are going to
+            // add them here to enrich the data (instead of storing it as a flat string).
+            if (pragma.Name == "sequence-region")
+            {
+                var sequenceParts = pragmaParts[1].Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                pragmaParts = new string[2]
+                {
+                    pragma.Name,
+                    string.Format("Chromosome={0};Start position={1};End position={2}", sequenceParts[0], sequenceParts[1], sequenceParts[2])
+                };
+            }
+
             // Some pragmas are just a string value.  We can identify those by their lack of a delimiter
             // for a tag key-value pair.
             if (!pragmaParts[1].Contains(KeyValueParser.TagKeyValueDelimiter))
