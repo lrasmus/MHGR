56727561297437ddc19bbc78f80e5a28cf1cfcfd
Luke Rasmussen <luke.rasmussen@gmail.com>
2015-10-11 20:30:31 -0500

    Layout patient results page

--- Viewer/Controllers/PatientController.cs
+++ Viewer/Controllers/PatientController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MHGR.Models.Repository;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,10 +10,25 @@ namespace Viewer.Controllers
 {
     public class PatientController : Controller
     {
-        // GET: Patient/Details/5
-        public PartialViewResult Details(int id)
+        private IPatientRepository Repository = null;
+
+        public PatientController()
         {
-            return PartialView("Results");
+            if (ConfigurationManager.AppSettings["Schema"].Equals("EAV", StringComparison.CurrentCultureIgnoreCase))
+            {
+                Repository = new MHGR.EAVModels.PatientRepository();
+            }
+            else
+            {
+                Repository = new MHGR.HybridModels.PatientRepository();
+            }
+        }
+
+        // GET: Patient/Details/5
+        public PartialViewResult Details(string id)
+        {
+            var patient = Repository.Search(id, 1).FirstOrDefault();
+            return PartialView("Results", patient);
         }
     }
 }
